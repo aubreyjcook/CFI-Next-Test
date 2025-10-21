5c77cefbc20a807cd2dfb029b1129eca42150200
aubreyjcook <aubreyjcook@gmail.com>
2025-10-21 04:32:10 -0500

    instantiate content layer

--- cfi-next-test/next.config.ts
+++ cfi-next-test/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { withContentlayer } from 'next-contentlayer'
+
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
+export default withContentlayer({});
 export default nextConfig;
